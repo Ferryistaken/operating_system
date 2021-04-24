35e0098d57726d59cc19a258754524d340f71b6a
Ferry <alessandro.ferrari.2004@gmail.com>
2021-04-24 16:23:19 -0400

    can now print to screen on x86_64

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,18 @@ fn panic(_info: &PanicInfo) -> ! {
     loop{}
 }
 
+static HELLO: &[u8] = b"Hello World! x86_64 Bare metal program test in the Rust programming language!";
+
 #[no_mangle]
 pub extern  "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop{}
 }
