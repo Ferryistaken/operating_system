3685d3b38a4d8d0bc3e5bcbeb4740ab356a1993e
Ferry <alessandro.ferrari.2004@gmail.com>
2021-04-24 21:45:57 -0400

    wrote basic vga text buffer driver to print to the screen in bios

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@
 
 use core::panic::PanicInfo;
 
+mod vga_buffer;
+
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop{}
@@ -12,14 +14,15 @@ static HELLO: &[u8] = b"VGA text mode test. x86_64 Bare metal program test in th
 
 #[no_mangle]
 pub extern  "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
+    // let vga_buffer = 0xb8000 as *mut u8;
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0x5;
-        }
-    }
+    // for (i, &byte) in HELLO.iter().enumerate() {
+    //     unsafe {
+    //         *vga_buffer.offset(i as isize * 2) = byte;
+    //         *vga_buffer.offset(i as isize * 2 + 1) = 0xE;
+    //     }
+    // }
+    vga_buffer::write_something();
 
     loop{}
 }
