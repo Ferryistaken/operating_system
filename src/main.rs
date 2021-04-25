7939c746ba00a55d1e7d67dc9a71652ae6ec482f
Ferry <alessandro.ferrari.2004@gmail.com>
2021-04-24 22:45:15 -0400

    wrote some macros to interface with the VGA text buffer, about to write tests

--- src/main.rs
+++ src/main.rs
@@ -6,24 +6,15 @@ use core::panic::PanicInfo;
 mod vga_buffer;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop{}
 }
 
-static HELLO: &[u8] = b"VGA text mode test. x86_64 Bare metal program test in the Rust programming language";
-
 #[no_mangle]
 pub extern  "C" fn _start() -> ! {
-    // let vga_buffer = 0xb8000 as *mut u8;
-
-    // for (i, &byte) in HELLO.iter().enumerate() {
-    //     unsafe {
-    //         *vga_buffer.offset(i as isize * 2) = byte;
-    //         *vga_buffer.offset(i as isize * 2 + 1) = 0xE;
-    //     }
-    // }
-    vga_buffer::write_something();
-
+    println!("Hello world using macros");
+    panic!("yo tf is going on");
     loop{}
 }
 
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,4 +1,11 @@
+//
 // This is the VGA text buffer driver
+//
+
+use core::fmt;
+use volatile::Volatile;
+use lazy_static::lazy_static;
+use spin::Mutex;
 
 // Enum of colors represented as u8
 #[allow(dead_code)]
@@ -49,7 +56,7 @@ const BUFFER_WIDTH: usize = 80;
 
 #[repr(transparent)]
 struct Buffer {
-    chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT],
+    chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],
 }
 
 // Writer type to write to the screen
@@ -59,19 +66,8 @@ pub struct Writer {
     buffer: &'static mut Buffer,
 }
 
-pub fn write_something() {
-    let mut writer = Writer {
-        column_position: 0,
-        color_code: ColorCode::new(Color::Yellow, Color::Red),
-        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
-    };
-
-    writer.write_byte(b'H');
-    writer.write_string("ello");
-    writer.write_string(" World!");
-}
-
 impl Writer {
+    /// Writes 1 byte to the screen
     pub fn write_byte(&mut self, byte: u8) {
         match byte {
             b'\n' => self.new_line(),
@@ -86,15 +82,16 @@ impl Writer {
 
                 let color_code = self.color_code;
                 // write ScreenChar to VGA buffer
-                self.buffer.chars[row][col] = ScreenChar {
+                self.buffer.chars[row][col].write(ScreenChar {
                     ascii_character: byte,
                     color_code,
-                };
+                });
                 self.column_position += 1;
             }
         }
     }
 
+    /// Writes a string(`&str`) to the screen
     pub fn write_string(&mut self, s: &str) {
         for byte in s.bytes() {
             match byte {
@@ -107,6 +104,66 @@ impl Writer {
     }
 
     fn new_line(&mut self) {
-        // TODO: Implement this
+        for row in 1..BUFFER_HEIGHT {
+            for col in 0..BUFFER_WIDTH {
+                let character = self.buffer.chars[row][col].read();
+                self.buffer.chars[row - 1][col].write(character);
+            }
+        }
+
+        self.clear_row(BUFFER_HEIGHT - 1);
+        self.column_position = 0;
     }
+
+    // Clears line by overwriting all its characters with a ' ' char
+    fn clear_row(&mut self, row: usize) {
+        let blank = ScreenChar {
+            ascii_character: b' ',
+            color_code: self.color_code,
+        };
+
+        for col in 0..BUFFER_WIDTH {
+            self.buffer.chars[row][col].write(blank);
+        }
+    }
+}
+
+/// This is to make my writer class work with default rust formatting
+/// so that I can print types
+impl fmt::Write for Writer {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.write_string(s);
+        Ok(())
+    }
+}
+
+// Global writer interface
+// I need to put it into lazy_static because rustc doesn't like 
+// pointers being initialized at compile times(since I use `static`)
+//
+// I need to put it in a Mutex since otherwise it would be immutable
+lazy_static! {
+    pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::LightGreen, Color::Black),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    });
+}
+
+// Macros
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::vga_buffer::_print(format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
+}
+
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments) {
+    use core::fmt::Write;
+    WRITER.lock().write_fmt(args).unwrap();
 }
