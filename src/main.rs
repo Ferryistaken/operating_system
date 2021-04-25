155533a4d20a35989eac37d7e572585e8fc52978
Ferry <alessandro.ferrari.2004@gmail.com>
2021-04-25 00:18:11 -0400

    added tests and qemu quits

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
 #![no_std]
 #![no_main]
 
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+// rename the functions created by test_runner, which is usually 
+// `main`. We can't use `main` because we specified `no_main`
+#![reexport_test_harness_main = "test_main"]
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+
+    // exit qemu
+    exit_qemu(QemuExitCode::Success);
+}
+
 use core::panic::PanicInfo;
 
 mod vga_buffer;
@@ -14,7 +31,36 @@ fn panic(info: &PanicInfo) -> ! {
 #[no_mangle]
 pub extern  "C" fn _start() -> ! {
     println!("Hello world using macros");
-    panic!("yo tf is going on");
+
+    #[cfg(test)]
+    test_main();
+
     loop{}
+}
+
+
+// tests
+#[test_case]
+fn trivial_assertion() {
+    print!("Trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}
+
+// This is to make sure that qemu exits once the tests finish
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
 }
 
