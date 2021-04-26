4d84bd9a8ae38d5be7c47486afbe5aa0ee5115c3
Ferry <alessandro.ferrari.2004@gmail.com>
2021-04-25 21:55:59 -0400

    readded tests, no longer need gui

--- src/main.rs
+++ src/main.rs
@@ -25,9 +25,21 @@ use core::panic::PanicInfo;
 
 mod vga_buffer;
 
+// Panic handler not in test mode, prints to VGA buffer
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
+    loop{}
+}
+
+// Panic handler in test mode, prints to serial
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
     loop{}
 }
 
